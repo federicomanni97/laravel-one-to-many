7bf34ce6ba08357a63a35e0ad20307d23c8afc6f
federicomanni97 <manni_federico@libero.it>
2024-01-16 14:26:25 +0100

    index categories foreach

---
+++ resources/views/admin/categories/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+@section('content')
+    <section class="container">
+        <h2 class="my-2 p-2">Projects</h2>
+        <!-- <p>section content</p> -->
+        @foreach ($categories as $category)
+        <!-- <p> <a href="{{route('admin.projects.show', $project->id)}}">{{$project->title}}</a></p> -->
+        <table class="table">
+        <tbody class="d-flex align-middle">
+            <tr>
+                <th scope="row"><a class="fw-normal text-decoration-none text-black" href="{{route('admin.categories.show', $category->id)}}">{{$$category->name}}</a></th>
+                <th><a href="{{route('admin.caregories.edit' , $category)}}" class="btn btn-primary">Edit</a></th>
+                <th>
+                    <form action="{{route('admin.categories.destroy' , $category)}}" method="POST">
+                    @csrf
+                    @method ('DELETE')
+                    <button type="submit" class="btn btn-danger">Delete</button>
+                    </form>
+                </th>
+            </tr>
+        </tbody>
+        </table>
+        @endforeach
+        <form class="p-2" action="{{route('admin.categories.create', $category->id)}}">
+            <button class=" btn btn-primary" >Create</button>
+        </form>
+    </section>
+@endsection
