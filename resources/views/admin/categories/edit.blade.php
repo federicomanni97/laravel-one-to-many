f6bae5a3154e303a0268d3560d5388f0c2a81583
federicomanni97 <manni_federico@libero.it>
2024-01-16 15:39:48 +0100

    style fixed

--- resources/views/admin/categories/edit.blade.php
+++ resources/views/admin/categories/edit.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.app')
 @section('content')
-<section class="container">
+<section class="container my-3">
     <h1>Edit {{$category->name}}</h1>
     <form action="{{ route('admin.categories.update', $category->slug) }}" method="POST">
         @csrf
         @method('PUT')
-        <div class="mb-3">
+        <div class="my-3">
             <label for="name">Name</label>
-            <input type="text" class="form-control @error('name') is-invalid @enderror" name="name" id="name"
+            <input type="text" class="form-control my-2 @error('name') is-invalid @enderror" name="name" id="name"
                 required minlength="3" maxlength="200" value="{{ old('name', $category->name) }}">
             @error('name')
                 <div class="invalid-feedback">{{ $message }}</div>
@@ -19,3 +19,4 @@
         </div>
     </form>
 </section>
+@endsection
